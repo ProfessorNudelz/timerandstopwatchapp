7af54fd737b64e151589cc93f2d4b93c0ffb11ba
Jesse Lyon <jesse.a.lyon+github@gmail.com>
2020-07-07 19:30:15 +1000

    Added a page with just a stopwatch

--- pages/stopwatch.js
+++ pages/stopwatch.js
@@ -1,27 +1,23 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
 import Clock from '../components/clock'
-import Stopwatch from '../components/stopwatch'
-import Countdown from '../components/countdown'
+import StopwatchFullLength from '../components/stopwatchFullLength'
 
 function index() {
   return (
     <Layout>
       <div className='container'>
         <div className='text-center'>
-          <Link href='/stopwatch'>
-            <a>Stopwatch</a>
+          <Link href='/'>
+            <a>Stopwatch and Timer</a>
           </Link>
         </div>
         <div className='row'>
           <Clock />
         </div>
         <div className='row'>
-          <div className='col-md-6'>
-            <Stopwatch />
-          </div>
-          <div className='col-md-6'>
-            <Countdown />
+          <div className='col-md-12'>
+            <StopwatchFullLength />
           </div>
         </div>
       </div>
